da92d574f6ad6eda3b22b21ed629d4cfc2e61b6c
Nieliton Rodrigues <nieliton.sousa3040@gmail.com>
2022-01-26 13:48:46 -0300

    Added filters by section

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 import { FetchApi } from "./api/Api.js";
-
+import { TemplateLoja } from "./models/templateLoja.js";
 // buscando a resposta da api
 const response = await FetchApi.getFetchApi()
+
+const getAll = new TemplateLoja("mainSection")
+
+const getAllButton = document.querySelector(".search-todos")
+getAll.createTemplate(response)
+
+getAll.createTemplateFilters(response)
+
+
+
+
+
+
+
+
